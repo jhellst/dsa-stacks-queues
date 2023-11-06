0bd66b9125ef8de97febda017dc6bb2e1efa0e82
Joshua <jhellst@gmail.com>
2023-11-06 15:08:19 -0800

    Completed ADT construction for Stack

--- stack.js
+++ stack.js
@@ -18,27 +18,46 @@ class Stack {
 
   /** push(val): add new value to the top of the stack. Returns undefined. */
 
-  push(val) {
+  // [1,2] 3 -> [1,2,3]
 
+  push(val) {
+    if (this.top) {
+      let newNode = new Node(val);
+      let prevTop = this.top;
+      this.top = newNode;
+      this.top.next = prevTop;
+    } else {
+      this.top = new Node(val);
+    }
+    this.size++;
   }
 
   /** pop(): remove the node from the top of the stack
    * and return its value. Should throw an error if the stack is empty. */
 
-  pop() {
+  // [1,2,3] -> [1,2] 3
 
+  pop() {
+    if (this.size === 0) throw new Error();
+
+    let removedNode = this.top;
+    this.top = this.top.next;
+    this.size--;
+    return removedNode.val;
   }
 
   /** peek(): return the value of the top node in the stack. */
 
   peek() {
+    if (this.size === 0) throw new Error();
+    return this.top.val;
 
   }
 
   /** isEmpty(): return true if the stack is empty, otherwise false */
 
   isEmpty() {
-
+    return (this.size === 0);
   }
 }
 
